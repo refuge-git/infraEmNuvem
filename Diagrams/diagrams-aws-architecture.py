ff61ce8a6e45f14c94bc2737ec2010b9b2d8b0d5
raulreis123 <raulgomesreis@gmail.com>
2025-11-23 19:20:29 -0300

    feature: Modificação de arquitetura de rede

--- Diagrams/diagrams-aws-architecture.py
+++ Diagrams/diagrams-aws-architecture.py
@@ -1,12 +1,12 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.network import VPC, InternetGateway, RouteTable, ELB
-from diagrams.aws.compute import EC2
+from diagrams.aws.compute import EC2, Lambda
 from diagrams.aws.database import RDS
 from diagrams.aws.storage import S3
 from diagrams.aws.management import Cloudwatch
 from diagrams.onprem.client import Users
 
-with Diagram("Arquitetura Web AWS", show=False, outformat="png", filename="diagrams/aws_web_architecture"):
+with Diagram("Arquitetura Web AWS com ETL de Imagens", show=False, outformat="png", filename="diagrams/aws_web_architecture_etl"):
     client = Users("Cliente")
     igw = InternetGateway("Internet Gateway")
 
@@ -18,10 +18,7 @@ with Diagram("Arquitetura Web AWS", show=False, outformat="png", filename="diagr
 
         igw >> rt 
 
-        elb_public = ELB("ELB Pública")
         elb_private = ELB("ELB Privada")
-
-        rt >> elb_public
 
         with Cluster("Availability Zone 1"):
             with Cluster("Sub-Rede 1"):
@@ -37,16 +34,33 @@ with Diagram("Arquitetura Web AWS", show=False, outformat="png", filename="diagr
                 be2 = EC2("Back-end 2")
                 db2 = RDS("Banco de Dados 2")
 
-        elb_public >> [fe1, fe2]
+        # Cliente acessa front-ends via rota
+        rt >> [fe1, fe2]
+
+        # Comunicação entre camadas
         fe1 >> elb_private
         fe2 >> elb_private
         elb_private >> be1
         elb_private >> be2
         fe1 >> be1 >> db1
         fe2 >> be2 >> db2
-        s3 = S3("Bucket S3")
+
+        # Buckets e Lambda do processo ETL
+        s3_raw = S3("bucket-refuge-img-raw")
+        lambda_etl = Lambda("funcao1_terraform (Lambda ETL)")
+        s3_trusted = S3("bucket-refuge-img-trusted")
+
+        # Fluxo ETL de imagem
+        be1 >> Edge(label="envia imagem") >> s3_raw
+        be2 >> Edge(label="envia imagem") >> s3_raw
+
+        s3_raw >> Edge(label="dispara trigger") >> lambda_etl
+        lambda_etl >> Edge(label="salva imagem processada") >> s3_trusted
+
+        # Backend consome imagem do bucket trusted
+        s3_trusted >> Edge(label="consome imagem processada") >> [be1, be2]
+
+        # Monitoramento
         cw = Cloudwatch("CloudWatch")
-        db1 >> s3
-        db2 >> s3
         db1 >> cw
         db2 >> cw
